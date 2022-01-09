4c0753e10e5f2329a45a1fa4774ad2a5b917afce
Qingqi Guan <gqqaaa@126.com>
2022-01-09 10:17:00 +0800

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,13 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("jdk8");
+        System.out.println("jdk8");
+        System.out.println("jdk8");
+        System.out.println("jdk8");
+        System.out.println("jdk8");
+        System.out.println("jdk8");
+        System.out.println("jdk8");
+        System.out.println("jdk8");
+        System.out.println("jdk8");
     }
 }
