e7742c32f7ef5fda466204640ce26ed7ff71ef9a
gqq <gqqaaa@126.com>
2022-01-09 10:17:53 +0800

    第二次git

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("jdk8");
+        System.out.println("jdk6");
+        System.out.println("jdk7");
     }
 }
